28cc8330ed4a481b34b85d78a18483d57fb1052b
NattanSL <43894421+nattanslobato@users.noreply.github.com>
2023-11-09 01:12:33 +0000

    Adicionando card.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,10 @@ function App() {
 
   return (
     <>
-    App
+      <div className="card">
+        <h2>Rick Sanchez</h2>
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+      </div>
     </>
   )
 }
