ae259489a210c3f3ca4a08799a5a79b0824a945a
NattanSL <43894421+nattanslobato@users.noreply.github.com>
2023-11-14 00:02:23 +0000

    Adicionando Cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Card from './components/Card/Card.jsx';
 
 function App() {
 
@@ -6,8 +7,14 @@ function App() {
     <>
       <div className="card">
         <h2>Rick Sanchez</h2>
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
+        </div>
         <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
       </div>
+      <Card />
     </>
   )
 }
