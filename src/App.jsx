d96a59e9e9f3f3921a95cdb9a665682aad57cfb2
NattanSL <43894421+nattanslobato@users.noreply.github.com>
2023-11-14 01:28:21 +0000

    Atualização do card e implementação do map

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,35 @@ import Card from './components/Card/Card.jsx';
 
 function App() {
 
+  const item1 = {
+    name: "Rick Sanchez",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"
+    
+  }
+  const item2 = {
+    name: "Morty Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"
+    
+  }
+  const item3 = {
+    name: "Summer Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"
+    
+  }
+
+  const item4 = {
+    name: "Beth Smith",
+    imageUrl: "https://rickandmortyapi.com/api/character/avatar/4.jpeg"
+    
+  }
+
+  const items = [item1, item2, item3, item4]
+
   return (
     <>
-      <div className="card">
-        <h2>Rick Sanchez</h2>
-        <div className="tags">
-          <div className="tag">Status: Vivo</div>
-          <div className="tag">Espécie: Humana</div>
-          <div className="tag">Origem: Terra C-137</div>
-        </div>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <Card />
+      {items.map(function(element){
+        return <Card item={element}/>
+      })}
     </>
   )
 }
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,7 +1,13 @@
-export default function Card(){
+export default function Card(props){
     return (
-        <div>
-            Card está funcionando!
+        <div className="card">
+        <h2>{props.item.name}</h2>
+        <div className="tags">
+          <div className="tag">Status: Vivo</div>
+          <div className="tag">Espécie: Humana</div>
+          <div className="tag">Origem: Terra C-137</div>
         </div>
+        <img src={props.item.imageUrl} />
+      </div>
     )
 }
