2a9c08c79394b42422351f2f5213fa0dfbdd69a0
NattanSL <43894421+nattanslobato@users.noreply.github.com>
2023-11-15 01:33:12 +0000

    Adicionando useParams para mostrar os ids.

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,5 +1,15 @@
+import { useParams } from "react-router-dom"
+
 export default function ReadById(){
+    const { id } = useParams()
+
+    const apiUrl = `https://rickandmortyapi.com/api/character/{id}`
+    //ToDo: Implementar o carregamento da API.
+    //ToDo: Salvar as informações em um estado.
+    //ToDo: Exibir essas informações no JSX.
+
+
     return (
-        <div>ReadById</div>
+        <div>ReadById: {id}</div>
     )
 }
