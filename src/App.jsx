f38a5321acc7dedcc1bd5b28805cd3a7f5a12fde
NattanSL <43894421+nattanslobato@users.noreply.github.com>
2023-11-15 01:14:34 +0000

    Criação do componente <ReadById>.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/Home/Home';
+import ReadById from './components/ReadById/ReadById';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<ReadById />} />
       </Routes>
     </BrowserRouter>
   )
---
+++ src/components/ReadById/ReadById.jsx
@@ -0,0 +1,5 @@
+export default function ReadById(){
+    return (
+        <div>ReadById</div>
+    )
+}
