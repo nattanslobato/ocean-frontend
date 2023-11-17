b4848901f95073a4d0b1e56c746a3d6da66dc879
nattanslobato <nattangmr2@gmail.com>
2023-11-16 21:48:52 -0300

    Criação e estilização do card de ReadById

--- src/components/ReadById/ReadById.jsx
+++ src/components/ReadById/ReadById.jsx
@@ -1,15 +1,62 @@
+import { useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
+import "./ReadById.css"
 
-export default function ReadById(){
+export default function ReadById() {
     const { id } = useParams()
 
-    const apiUrl = `https://rickandmortyapi.com/api/character/{id}`
-    //ToDo: Implementar o carregamento da API.
-    //ToDo: Salvar as informações em um estado.
+    const [item, setItem] = useState({})
+
+    async function carregarDadosApi() {
+        const apiUrl = `https://rickandmortyapi.com/api/character/${id}`
+
+        const response = await fetch(apiUrl)
+
+        const body = await response.json()
+        
+        setItem(body)
+
+        //ToDo: Implementar o carregamento da API.
+        //ToDo: Salvar as informações em um estado.
+
+    }
+    useEffect(function () {
+        carregarDadosApi()
+    }, [])
     //ToDo: Exibir essas informações no JSX.
 
 
     return (
-        <div>ReadById: {id}</div>
+<div className="main">
+      <div className="image-wrapper">
+        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" alt="Imagem do Personagem" />
+      </div>
+
+      <div className="info">
+        <div className="info-tag">Status: Alive</div>
+
+        <div className="info-title">Rick Sanchez</div>
+
+        <div className="info-row">
+          <div className="info-label">Species:</div>
+          <div className="info-text">Human</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Gender:</div>
+          <div className="info-text">Male</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Origin:</div>
+          <div className="info-text">Earth (C-137)</div>
+        </div>
+
+        <div className="info-row">
+          <div className="info-label">Location:</div>
+          <div className="info-text">Citadel of Ricks</div>
+        </div>
+      </div>
+    </div>
     )
 }
